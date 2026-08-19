a3b17e1bd3c690e6877d86d47866a8fd0c03ba19
Curious-being99 <256735887+Curious-being99@users.noreply.github.com>
2026-08-20 02:28:04 +0300

    Change

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   {
-    ignores: ['dist/**', 'node_modules/**'],
+    ignores: ['dist/**', 'node_modules/**', 'android/**'],
   },
   {
     files: ['**/*.{ts,tsx}'],
