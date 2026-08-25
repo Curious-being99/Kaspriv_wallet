4d7d13c5e5c82446171207a64ee8162fdfa3ea14
Curious-being99 <256735887+Curious-being99@users.noreply.github.com>
2026-08-25 20:01:21 +0300

    Test script remove

--- test-spk.js
+++
@@ -1,4 +0,0 @@
-import { payToScriptHashScript } from '@kasdk/nodejs';
-const scriptHash = new Uint8Array(32); // All zeros
-const script = payToScriptHashScript(scriptHash);
-console.log(Buffer.from(script.script).toString('hex'));
